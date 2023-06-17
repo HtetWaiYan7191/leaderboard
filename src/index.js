cd1b441c6fd5711ea3ad46eb935942cfe614d2ea
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-06-17 21:09:47 +0630

    Implement create game function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
 import './style.css';
+
+const createGame = async () => {
+  const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/';
+  const gameName = {
+    name: 'My cool new game',
+  };
+  const requestOptions = {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json; charset=UTF-8',
+    },
+    body: JSON.stringify(gameName),
+  };
+
+  try {
+    const response = await fetch(url, requestOptions);
+    const data = await response.json();
+    console.log(data);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+createGame();
