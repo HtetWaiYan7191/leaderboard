e0e08564727d7ce8b3d4ec4678c13a0fd26d8e00
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-06-17 21:45:14 +0630

    Implement function in refresh button to get data from API

--- src/index.js
+++ src/index.js
@@ -20,9 +20,29 @@ const createGame = async () => {
     const regex = /Game with ID: ([A-Za-z0-9]+)/;
     const match = gameIdStr.match(regex);
     const gameId = match[1];
+    return gameId;
   } catch (err) {
     console.log(err);
+    throw err;
   }
 };
+
+const getScore = async () => {
+  try {
+    const gameId = await createGame();
+    const response = await fetch(`https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameId}/scores`);
+    const data = await response.json();
+    return data.result;
+  } catch (error) {
+    console.error('Error fetching score:', error);
+    throw error;
+  }
+};
+
+const refreshBtn = document.getElementById('refresh-button');
+refreshBtn.addEventListener('click', async () => {
+  const result = await getScore();
+  console.log(result);
+});
 
 createGame();
